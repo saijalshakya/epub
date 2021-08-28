c853f18080ede987a2c36280618704aa6ab55a46
Saijal Shakya <tech@scssconsulting.com>
2021-08-28 19:49:35 +0545

    init

---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <div class="sidebar">
+        <a class="active" href="/index.html">Home</a>
+        <a href="/table-of-content.html">Table of Content</a>
+        <a href="#contact">Contact</a>
+        <a href="#about">About</a>
+    </div>
+    )
+}
+
+export default Navigation;
