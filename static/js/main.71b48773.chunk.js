5d7114255b480fef2425bc8903b2899b94d685e0
Saijal Shakya <tech@scssconsulting.com>
2021-08-30 13:28:02 +0545

    Updates

--- static/js/main.71b48773.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpepub-disney"]=this["webpackJsonpepub-disney"]||[]).push([[0],{10:function(e,n,t){"use strict";t.r(n);var s=t(1),c=t.n(s),r=t(3),a=t.n(r),i=(t(8),t(0));var d=function(){return Object(i.jsx)("div",{className:"App",children:Object(i.jsx)("header",{className:"App-header"})})};a.a.render(Object(i.jsx)(c.a.StrictMode,{children:Object(i.jsx)(d,{})}),document.getElementById("root"))},8:function(e,n,t){}},[[10,1,2]]]);
-//# sourceMappingURL=main.71b48773.chunk.js.map
